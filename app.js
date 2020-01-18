d2e4495be540b35544737b242791230301573552
travischantravis <chancheukyin4463@gmail.com>
2020-01-18 14:06:35 -0800

    basic template

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.set("view engine", "ejs");
+
+// @route GET /
+// @desc Loads form
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
