3b8463f857dd31583fe0aee1d3ab5100b63e5445
travischantravis <chancheukyin4463@gmail.com>
2020-01-18 22:16:25 -0800

    first backend step

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,13 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const path = require("path");
-const bodyParser = require("body-parser");
-const crypto = require("crypto");
-const mongoose = require("mongoose");
-const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
-const methodOverride = require("method-override");
+
+const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("upload");
