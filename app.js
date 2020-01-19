6e3dfb5bfb22c20f3348f84804b929ccdec1f2fc
travischantravis <chancheukyin4463@gmail.com>
2020-01-19 06:46:00 -0800

    tried google api

--- app.js
+++ app.js
@@ -259,6 +259,24 @@ app.get("/post", async (req, res) => {
   }
 });
 
+async function quickstart() {
+  // Imports the Google Cloud client library
+  const vision = require("@google-cloud/vision");
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: "./APIKey.json"
+  });
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection("./public/img/hydro-flask.jpg");
+  const labels = result.labelAnnotations;
+  console.log("Labels:");
+  labels.forEach(label => console.log(label.description));
+}
+
+quickstart();
+
 // Routes
 // app.use("/upload", require("./routes/uploads"));
 // app.use("/post", require("./routes/posts"));
